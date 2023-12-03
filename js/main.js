9355f370c4141245cb50ce1124b48ee9f1c9144a
VilliamK <vipebubu@gmail.com>
2023-12-03 16:30:59 +0200

    HTML, CSS JA JS -PÄIVITYSTÄ

--- js/main.js
+++ js/main.js
@@ -1,13 +1,53 @@
 'use strict';
 /* 1. show map using Leaflet library. (L comes from the Leaflet library) */
 
-const map = L.map('map', {tap: false});
-L.tileLayer('https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
-  maxZoom: 20,
-  subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+// Haetaan kartta ja keskitetään se aluksi Lontooseen
+const map = L.map('map').setView([51.505, -0.09], 13);
+
+// Pinnamerkitsijän lisääminen karttaan
+const marker = L.marker([51.5, -0.09]).addTo(map);
+
+// Ympyrämerkitsijän lisääminen karttaan
+const circle = L.circle([51.508, -0.11], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
 }).addTo(map);
-map.setView([60, 24], 7);
- 
+
+// Kolmiomerkitsijän lisääminen karttaan
+const polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(map);
+
+// Pop-up toiminnot markkereille, openPopup()-metodi avaa ikkunan välittömästi
+marker.bindPopup("I am a marker.").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon.");
+
+// Popup-ikkuna toiminto karttaan
+const popup = L.popup();
+
+
+// TileLayerin lisääminen (openstreetmapista)
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+
+// Metodi, joka palauttaa paikan koordinaatin käyttäjän painaessa karttaa
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+// Tapahtumakäsittelijä onMapClick -metodille
+map.on('click', onMapClick);
+
 
 // global variables
 
