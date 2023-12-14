97a8d2df87837b006b16904454a93449d659b0dd
VilliamK <vipebubu@gmail.com>
2023-12-14 13:45:54 +0200

    Gameover korjattu

--- pythonia/yhteys.py
+++ pythonia/yhteys.py
@@ -3,10 +3,10 @@ import mysql.connector
 yhteys = mysql.connector.connect(
     host='127.0.0.1',
     port= 3306,
-    database='prokkis2',
-    user='root',
-    password='veetikol',
+    database='projekti2',
+    user='user1',
+    password='password1',
     autocommit=True
     )
 
-"""Huom. Vaihtakkee omat kirjautumistunnukset"""
+"""Huom. Vaihtakkee omat kirjautumistunnukset, muistakaa laittaa myös oikea projekti databaseksi"""
